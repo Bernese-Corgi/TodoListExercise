21023c69ff811a29b5a977db192adf0fe7d7407f
Bernese-Corgi <jnyng0621@gmail.com>
2021-02-09 19:26:53 +0900

    remove 버튼 클릭 시 li요소의 전체 삭제 - 이벤트 위임

--- TodoList_v1/js/app.js
+++ TodoList_v1/js/app.js
@@ -3,10 +3,6 @@ const $inputTodo = document.querySelector('.input-todo');
 const $add = document.querySelector('.add');
 const $todos = document.querySelector('.todos');
 
-const handler = () => {
-  console.log('clicked!');
-};
-
 const addTodo = () => {
   const value = $inputTodo.value;
   if (!value) return;
@@ -21,9 +17,6 @@ const addTodo = () => {
   $remove.classList.add('remove');
   $remove.textContent = 'x';
 
-  // 각각의 remove 버튼 요소의 이벤트 핸들러 프로퍼티에 모두 함수가 달려있다.
-  $remove.onclick = handler;
-
   $li.appendChild($checkbox);
   $li.appendChild($span);
   $li.appendChild($remove);
@@ -37,9 +30,9 @@ $form.onsubmit = e => {
   e.preventDefault();
   addTodo();
 };
-// remove 버튼이 생성된 이후에 실행되는 이벤트이므로, addTodo 함수 안에서 remove버튼을 생성하는 코드 이후에 지정해야햔다.
-document.querySelectorAll('.remove').forEach($btn => {
-  $btn.onclick = () => {
-    console.log('clicked');
-  };
-});
+
+$todos.onclick = e => {
+  if (!e.target.classList.contains('remove')) return;
+  // console.log(e.target);
+  e.target.parentNode.remove();
+};
