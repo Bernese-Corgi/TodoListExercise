f6e7db1131c044fd193ad039362ac4e058ada946
Bernese-Corgi <jnyng0621@gmail.com>
2021-02-09 17:12:06 +0900

    value 값이 없으면 아무런 동작도 하지 않음

--- TodoList_v1/js/app.js
+++ TodoList_v1/js/app.js
@@ -4,11 +4,10 @@ const $todos = document.querySelector('.todos');
 
 $add.onclick = () => {
   const value = $inputTodo.value;
-  // 객체 디스트럭칭 할당
-  // const { value } = $inputTodo;
+
   if (!value) return; // value가 빈 문자열이면 아무것도 하지 않겠다.
   console.log(value);
   $todos.innerHTML += `<li>${value}</li>`
-  $inputTodo.value = ''; // 이 방식으로는 지워지지 않는다. 변수에 있는 문자를 빈 문자열로 재할당했을뿐.
+  $inputTodo.value = '';
   $inputTodo.focus();
 };
