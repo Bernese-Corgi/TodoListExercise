f5f2e811a4ecf613c82c50f3fce965ae6ffc41a5
Bernese-Corgi <jnyng0621@gmail.com>
2021-02-09 17:25:27 +0900

    enter키 입력 시 투두리스트 추가

--- TodoList_v1/js/app.js
+++ TodoList_v1/js/app.js
@@ -4,9 +4,29 @@ const $todos = document.querySelector('.todos');
 
 $add.onclick = () => {
   const value = $inputTodo.value;
+  // console.log(value);
 
-  if (!value) return; // value가 빈 문자열이면 아무것도 하지 않겠다.
-  console.log(value);
+  if (!value) return;
+
+  $todos.innerHTML += `<li>${value}</li>`
+  $inputTodo.value = '';
+  $inputTodo.focus();
+};
+
+// 엔터 키를 눌렀을 때 value 값이 li 요소로 추가
+$inputTodo.onkeyup = e => {
+  // 이벤트 객체를 브라우저가 만드는데, 이 이벤트 객체는 이벤트 핸들러의 첫번째 인수로 준다.
+  // 이벤트 객체를 볼 것이므로, 인수를 지정했다.
+
+  // key 값이 enter가 아니면 아무 동작도 하지 않는다.
+  if (e.key !== 'Enter') return;
+
+  // key 값이 enter인 경우 아래 동작을 실행한다. 
+  const value = $inputTodo.value;
+
+  if (!value) return;
+
+  console.log(e.key);
   $todos.innerHTML += `<li>${value}</li>`
   $inputTodo.value = '';
   $inputTodo.focus();
