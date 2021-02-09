29b5077293a4772e8f62cb6543917077997a2a1f
Bernese-Corgi <jnyng0621@gmail.com>
2021-02-09 19:01:25 +0900

    remove event 함수의 위치 선정

--- TodoList_v1/js/app.js
+++ TodoList_v1/js/app.js
@@ -3,8 +3,11 @@ const $inputTodo = document.querySelector('.input-todo');
 const $add = document.querySelector('.add');
 const $todos = document.querySelector('.todos');
 
-const addTodo = () => {
+const handler = () => {
+  console.log('clicked!');
+};
 
+const addTodo = () => {
   const value = $inputTodo.value;
   if (!value) return;
 
@@ -18,6 +21,9 @@ const addTodo = () => {
   $remove.classList.add('remove');
   $remove.textContent = 'x';
 
+  // 각각의 remove 버튼 요소의 이벤트 핸들러 프로퍼티에 모두 함수가 달려있다.
+  $remove.onclick = handler;
+
   $li.appendChild($checkbox);
   $li.appendChild($span);
   $li.appendChild($remove);
@@ -27,15 +33,13 @@ const addTodo = () => {
   $inputTodo.focus();
 };
 
-// $add.onclick = addTodo;
-
-// $inputTodo.onkeyup = e => {
-//   if (e.key !== 'Enter') return;
-
-//   addTodo();
-// };
-
 $form.onsubmit = e => {
   e.preventDefault();
   addTodo();
 };
+// remove 버튼이 생성된 이후에 실행되는 이벤트이므로, addTodo 함수 안에서 remove버튼을 생성하는 코드 이후에 지정해야햔다.
+document.querySelectorAll('.remove').forEach($btn => {
+  $btn.onclick = () => {
+    console.log('clicked');
+  };
+});
